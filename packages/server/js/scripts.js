bd7c43722965a3c0144599e59184cf10629fdcf8
agarcia-imt <agarcia.imt@gmail.com>
2021-01-17 17:23:19 -0600

    console scripts 1

---
+++ packages/server/js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+  $('#mycarousel').carousel({ interval: 2000 });
+  $('#carouselButton').click(() => {
+    if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+      $('#mycarousel').carousel('pause');
+      $('#carouselButton').children('span').removeClass('fa-pause');
+      $('#carouselButton').children('span').addClass('fa-play');
+    } else {
+      $('#mycarousel').carousel('cycle');
+      $('#carouselButton').children('span').removeClass('fa-play');
+      $('#carouselButton').children('span').addClass('fa-pause');
+    }
+  });
+
+  // Login
+  $('#triggerLogin').click(() => {
+    $('#loginModal').modal('toggle');
+  });
+
+  // Reserve Table
+  $('#triggerReserve').click(() => {
+    $('#reserveModal').modal('toggle');
+  });
+});
